2c9552f197ee64628a26512481908e9f7ef5e8f0
Jose Roberto <s22030110@itsch.edu.mx>
2026-03-16 15:41:54 -0600

    commit inicial

---
+++ src/proyecto1/Proyecto1.java
@@ -0,0 +1,17 @@
+package proyecto1;
+// @author emial
+
+import javax.swing.JFrame;
+
+
+public class Proyecto1 {
+
+    public static void main(String[] args) {
+    Ventana ventana = new Ventana();
+        ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        ventana.setSize(500, 500);
+        ventana.setVisible(true);
+        ventana.setResizable(false);
+    }
+    
+}
